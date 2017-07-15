188d0c547bab1a19067a1ac9c754411b54261fcb
Ahmad Tahani <ahmad.tahani@gmail.com>
2017-07-15 08:56:45 +0430

    configure ESLint on project

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import {AppContainer} from 'react-hot-loader';
 import App from './components/App';
 
 const run = Component => {
@@ -8,27 +8,24 @@ const run = Component => {
   if (process.env.NODE_ENV === 'development') {
     ReactDOM.render(
       <AppContainer>
-      <Component  />
-    </AppContainer>,
-      document.getElementById('app')
-    );
+        <Component />
+      </AppContainer>, document.getElementById('app'));
   } else {
     ReactDOM.render(
-      <Component  />,
-      document.getElementById('app')
-    );
+      <Component />, document.getElementById('app'));
   }
 };
 
 run(App);
-
 
 /**
  * hot module replacement in development
  */
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./components/App', () => {
-    run(App);
-  });
+  module
+    .hot
+    .accept('./components/App', () => {
+      run(App);
+    });
 }
