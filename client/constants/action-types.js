6bba0850526e8df53eaf56140a1170b607483333
Ahmad Tahani <ahmad.tahani@gmail.com>
2017-07-15 19:37:13 +0430

    setAccessToken after accessToken taken

--- client/constants/action-types.js
+++ client/constants/action-types.js
@@ -3,3 +3,4 @@ export const SE_LAST_PATH_NAME = 'SE_LAST_PATH_NAME';
 
 // user action
 export const GE_ACCESS_TOKEN = 'GE_ACCESS_TOKEN';
+export const SE_ACCESS_TOKEN = 'SE_ACCESS_TOKEN';
--- client/reducers/index.jsx
+++ client/reducers/index.jsx
@@ -1,13 +1,15 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
 import app from './app';
+import user from './user';
 
 // combine multiple reducers
 const appReducer = combineReducers({
   // you can add many reducer
   app,
+  user,
   // Add the reducer to your store on the `router` key
-  router: routerReducer,
+  router: routerReducer
 });
 
 export default appReducer;
