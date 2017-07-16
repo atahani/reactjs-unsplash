bad20a86382c6152cc6a1e07442456c0929173e4
Ahmad Tahani <ahmad.tahani@gmail.com>
2017-07-16 17:38:17 +0430

    add reudx-form package and add related reducer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Dialog
+## redux form - add or edit collection
+
+[http://redux-form.com/7.0.0/docs/GettingStarted.md/](redux-form)
+
+add `redux-form` package
+
+```
+yarn add redux-form@^6.8.0
+```
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
+import { reducer as formReducers } from 'redux-form';
 import app from './app';
 import user from './user';
 import items from './items';
@@ -12,7 +13,9 @@ const appReducer = combineReducers({
   user,
   items,
   // Add the reducer to your store on the `router` key
-  router: routerReducer
+  router: routerReducer,
+  // store form state by redux-form
+  form: formReducers,
 });
 
 const rootReducer = (state, action) => {
