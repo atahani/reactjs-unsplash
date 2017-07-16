8585a03f7f38fb8acd75fb0d768824ffeac1fd0e
Ahmad Tahani <ahmad.tahani@gmail.com>
2017-07-17 01:43:39 +0430

    add redux-mock-store package and write some test

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## test photo saga
+## test by redux-mock-store
+
+[http://arnaudbenard.com/redux-mock-store/](redux-mock-store)
+
+add `redux-mock-store` package
+
+```
+yarn add redux-mock-store --dev
+```
---
+++ client/__tests__/store.test.js
@@ -0,0 +1,28 @@
+import configureStore from 'redux-mock-store';
+import {setLastPathName} from '../actions/app';
+import {SE_LAST_PATH_NAME} from '../constants/action-types';
+import appReducer from '../reducers/app';
+
+/**
+ * http://arnaudbenard.com/redux-mock-store/
+ * http://redux.js.org/docs/recipes/WritingTests.html
+ */
+describe('test and dispatch the actions to store', () => {
+
+  it('dispatch the setLastPathName action to store', () => {
+    const mockStore = configureStore([]);
+    const store = mockStore({});
+    store.replaceReducer(appReducer);
+
+    store.dispatch(setLastPathName('/auth'));
+
+    const actions = store.getActions();
+    expect(actions).toEqual([
+      {
+        type: SE_LAST_PATH_NAME,
+        pathName: '/auth'
+      }
+    ]);
+
+  });
+});
