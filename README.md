b644c1d7d13be8db0f9057cf1bab07243bd0bbb8
Ahmad Tahani <ahmad.tahani@gmail.com>
2017-07-17 09:30:30 +0430

    add jest-styled-components & react-test-renderer to testing media func in style/util via snapshot

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-## test by redux-mock-store
+## Snapshot testing style util
 
-[http://arnaudbenard.com/redux-mock-store/](redux-mock-store)
+[http://facebook.github.io/jest/docs/en/snapshot-testing.html#content](Snapshot Testing)
 
-add `redux-mock-store` package
+1.add [https://github.com/styled-components/jest-styled-components](jest-styled-components) to snapshot component with style
 
 ```
-yarn add redux-mock-store --dev
+yarn add jest-styled-components@^3.3.1 --dev
+```
+
+2.add [https://github.com/facebook/react/tree/master/packages/react-test-renderer](react-test-renderer) to render react component to pure javascript objects
+
+```
+yarn add react-test-renderer@^15.6.1 --dev
 ```
---
+++ client/__tests__/style/util.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import {media} from '../../style/util';
+
+const Div = styled.div `
+  ${media.giant `
+    width: 1170px;
+  `}
+  ${media.desktop `
+    width: 992px;
+  `}
+  ${media.tablet `
+    width: 768px;
+  `}
+  ${media.phone `
+    width: 376px;
+  `}
+`;
+
+describe('Test style Util', () => {
+  describe('media function to generate CSS media query', () => {
+    it('should generate the media query base on phone size', () => {
+      const wrapper = renderer
+        .create(<Div />)
+        .toJSON();
+      expect(wrapper).toMatchStyledComponentsSnapshot();
+    });
+  });
+});
